fbc73dbe6ca96160e0f1da004a3a262766277d33
Dave Maison <davemaison@gmail.com>
2019-05-15 17:18:22 -0400

    replace classes with hooks

--- src/components/Grid/Note/Note.js
+++ src/components/Grid/Note/Note.js
@@ -3,28 +3,25 @@ import { GRID_OPTIONS } from '../../../constants/grid';
 import PropTypes from 'prop-types';
 import './style.css';
 
-const Note = props => {    
-    return (
-        <mark className="notes">
-            {
-                GRID_OPTIONS.map(
-                    option => <span 
-                                className={ props.values.includes( option ) ? 'active' : null }
-                                data-value={ option }
-                                key={ option } />
-                )
-            }
-        </mark>
-    );
-
-}
+const Note = props => (
+    <mark className="notes">
+        {
+            GRID_OPTIONS.map(
+                option => <span 
+                            className={ props.values.includes( option ) ? 'active' : null }
+                            data-value={ option }
+                            key={ option } />
+            )
+        }
+    </mark>
+);
 
 Note.defaultProps = {
     values: []
 };
 
 Note.propTypes = {
-    values: PropTypes.array
+    values: PropTypes.arrayOf( PropTypes.number )
 };
 
 export default Note;
--- src/containers/Grid/Grid.js
+++ src/containers/Grid/Grid.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -14,45 +14,42 @@ import './style.css';
  * @returns {JSX}
  * @description Displays the menu controls for the game
  */
-class Grid extends React.PureComponent {
+const Grid = props => {
 
-    static propTypes = {
-        grid: PropTypes.arrayOf(
-            PropTypes.shape({
-                column: PropTypes.number.isRequired,
-                row: PropTypes.number.isRequired,
-                section: PropTypes.number.isRequired
-            })
-        ),
-        reset: PropTypes.func.isRequired
-    }
-
-    componentDidMount(){
-        this.props.reset();
-    }
-
-    render(){
-        return (
-            <main className="grid">
-                {
-                    GRID_OPTIONS.map(
-                        section => (
-                            <Section key={ 'section-' + section }>
-                                {
-                                    this
-                                        .props
-                                        .grid
-                                        .filter( cell => cell.section === ( section - 1 ) )
-                                        .map( ( cell, index ) => <Cell { ...cell } key={ `section-${ section }cell-${ index }` } /> )
-                                }
-                            </Section>
-                        )
+    useEffect( () => {
+        props.reset();
+    }, []);
+    
+    return (
+        <main className="grid">
+            {
+                GRID_OPTIONS.map(
+                    section => (
+                        <Section key={ 'section-' + section }>
+                            {
+                                props
+                                    .grid
+                                    .filter( cell => cell.section === ( section - 1 ) )
+                                    .map( ( cell, index ) => <Cell { ...cell } key={ `section-${ section }cell-${ index }` } /> )
+                            }
+                        </Section>
                     )
-                }
-            </main>
-        )
-    }
+                )
+            }
+        </main>
+    );
 }
+
+Grid.propTypes = {
+    grid: PropTypes.arrayOf(
+        PropTypes.shape({
+            column: PropTypes.number.isRequired,
+            row: PropTypes.number.isRequired,
+            section: PropTypes.number.isRequired
+        })
+    ),
+    reset: PropTypes.func.isRequired
+};
 
 const mapStateToProps = state => ({
     grid: state.app.grid
