0d66defc03c543a4c65a75292d811f05bcf99fd8
dmaison <davemaison@gmail.com>
2023-05-05 19:51:24 -0400

    a little more progress

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,13 +1,27 @@
 import Container from '@mui/material/Container'
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import EditIcon from '@mui/icons-material/Edit';
+import Avatar from '@mui/material/Avatar';
+import { useSelector } from 'react-redux';
 
 const Controls = props => {
 
+    const length = useSelector( state => state.playArea.limit );
+
     return (
-        <Container>
-            <BottomNavigation showLabels>
-                <BottomNavigationAction label="1" />
+        <Container sx={{ mt: 3 }}>
+            <BottomNavigation>
+                {
+                    Array.from({ length }).map(
+                        ( _, index ) => (
+                            <BottomNavigationAction 
+                                icon={ <Avatar>{ ( index + 1 ) }</Avatar> } 
+                                key={ `input-${ index }` } />
+                        )
+                    )   
+                }
+                <BottomNavigationAction label="Note Mode" icon={ <EditIcon /> } />
             </BottomNavigation>
         </Container>
     )
