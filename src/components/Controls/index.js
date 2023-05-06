e75e1348fc2d9aec6eaed7b93d7d1664b504a101
dmaison <davemaison@gmail.com>
2023-05-05 21:24:59 -0400

    control placeholders

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -2,26 +2,35 @@ import Container from '@mui/material/Container'
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import EditIcon from '@mui/icons-material/Edit';
+import ClearIcon from '@mui/icons-material/Clear';
 import Avatar from '@mui/material/Avatar';
+import { useTheme } from '@mui/material/styles';
 import { useSelector } from 'react-redux';
 
 const Controls = props => {
 
-    const length = useSelector( state => state.playArea.limit );
+    const length = useSelector( state => state.playArea.limit ),
+    theme = useTheme();
 
     return (
         <Container sx={{ mt: 3 }}>
-            <BottomNavigation>
+            <BottomNavigation showLabels>
                 {
                     Array.from({ length }).map(
                         ( _, index ) => (
                             <BottomNavigationAction 
-                                icon={ <Avatar>{ ( index + 1 ) }</Avatar> } 
-                                key={ `input-${ index }` } />
+                                icon={ 
+                                    <Avatar sx={{ bgcolor: theme.palette.primary.main }}>
+                                        { ( index + 1 ) }
+                                    </Avatar> 
+                                } 
+                                key={ `input-${ index }` }
+                                />
                         )
                     )   
                 }
-                <BottomNavigationAction label="Note Mode" icon={ <EditIcon /> } />
+                <BottomNavigationAction label={ `Notes: Off` } icon={ <EditIcon sx={{ color: theme.palette.primary.main }} /> } />
+                <BottomNavigationAction label="Clear Cell" icon={ <ClearIcon sx={{ color: theme.palette.primary.main }} /> } />
             </BottomNavigation>
         </Container>
     )
